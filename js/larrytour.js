a3169c5211c349f89ce6c9824bb0fb60bafaed2c
tinaronelle <41747393+tinaronelle@users.noreply.github.com>
2019-04-24 14:31:18 -0400

    Add files via upload

---
+++ js/larrytour.js
@@ -0,0 +1,15 @@
+$("[data-toggle=popover]").popover();$('#openBtn').click(function(){
+	$('#myModal').modal({show:true})
+});$('#myTab a').click(function (e) {
+	 e.preventDefault();
+	 $(this).tab('show');
+});
+
+$(function () {
+$('#myTab a:last').tab('show');
+})
+$("[data-toggle=tooltip]").tooltip();
+
+$("[data-toggle=tooltip]").tooltip();
+
+$("[data-toggle=tooltip]").tooltip();
